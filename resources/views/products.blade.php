091d7c2c9060060dc4aa01e4251c63990e30d987
DiegoCosta <d.costa1990@outlook.com>
2017-11-25 16:06:42 -0300

    Las fotos siguen sin verse, el paginado esta hecho :) solo falta el css de las cosas(a ponerlo lindo), FALTA EL LOGOUT

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -10,5 +10,10 @@
             @endforeach
           </ul>
         </article>
+        @if (count($products))
+          <div class="col-xs-12 row justify-content-start" style="text-align:center">
+            {{$products->links()}}
+          </div>
+        @endif
       </section>
     @endsection
