853776c71dbe49f6ea08854d3c126298f505794b
MMXLV <elpaladindx@hotmail.com>
2017-11-26 14:56:00 -0300

    Fix de img, logout

--- resources/views/baseLayout.blade.php
+++ resources/views/baseLayout.blade.php
@@ -20,21 +20,24 @@
     <div class="container">
       <header class="main-head">
         <div class="logo">
-          <a href="/"><img src="images/logo.png" alt="Logo"></a>
+          <a href="/"><img src="{{ asset('images/logo.png')}}" alt="Logo"></a>
         </div>
         <div>
           <!-- nav de escritorio //-->
           <ul class="nav-bar-lg">
             <li><a class="dropbtn" href="/">Home</a></li>
-            @if (!isset($_SESSION['usuarioLogueado']))
+            @if (!Auth::check())
               <li><a class="dropbtn" href="{{ route('login') }}">Login</a></li>
               <li><a class="dropbtn" href="{{ route('register') }}">Register</a></li>
+              <li><a class="dropbtn" href="{{ route('products') }}">Products</a></li>
             @else
-              <li><a class="dropbtn" href="panel.php">Panel</a></li>
+              <li><a class="dropbtn" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">Logout</a></li>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  {{ csrf_field() }}
+              </form>
+              <li><a class="dropbtn" href="{{ route('addproducts') }}">Upload new product</a></li>
             @endif
             <li><a class="dropbtn" href="{{ route('faqs') }}">FAQs</a></li>
-            <li><a class="dropbtn" href="{{ route('products') }}">Products</a></li>
-            <li><a class="dropbtn" href="{{ route('addproducts') }}">Upload new product</a></li>
 
           </ul>
         </div>
